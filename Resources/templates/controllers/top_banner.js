8a6fd781baa10ad2275b153a41a52b0b24d89b41
Antonio kaplan <antonio@innovent.tv>
2012-10-22 04:44:33 -0700

    getting rid of the default

--- Resources/templates/controllers/top_banner.js
+++ Resources/templates/controllers/top_banner.js
@@ -1,26 +1,28 @@
 module.exports = function(view) {
   var Repo = nrequire('/lib/repo');
-      
+
   var url = 'http://www.doctorswithoutborders.com/',
-  
+
       openDonateLink = function() {
         Ti.Platform.openURL(url);
       },
-  
+
       updateTopbarMessage = function(val) {
         if(!isIPad) { return; }
         if(val) { view.title_message.text = val; }
       },
-  
+
       updateLogo = function(val) {
         if(val) { view.logo.image = val; }
       },
-  
+
       pullAcsInfo = function() {
         Repo.getTopBarMessageAndLogoAndDonateUrl(function(e){
           updateTopbarMessage(e.topbar_message);
-          updateLogo('/foo.png');
-          url = e.donate_url;
+//          updateLogo(e.logo_url);
+//          url = e.donate_url;
+          updateLogo('/images/top_logo.png');
+          url = 'https://npo.networkforgood.org/Donate/Donate.aspx?npoSubscriptionId=1001210';
         });
       };
 
